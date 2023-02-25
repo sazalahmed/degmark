353110e00e0161308a8411d660f596a85493f950
freelancersazal <freelancersazal@gmail.com>
2023-02-25 22:12:33 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+$(function () {
+    "use strict";
+
+
+
+
+    //======NICE SELECT========
+    $('.select_js').niceSelect();
+
+
+    //=======COUNTER JS=======
+    $('.counter').countUp();
+
+
+
+
+
+
+
+    //========= summer note js =========
+    // $(document).ready(function () {
+    //     $('.summer_note').summernote();
+    // });
+
+
+
+
+    //=======TESTI SLIDER======
+    $('.testi_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+
+
+
+
+});
